d4c39ef4a615c4aa486d81f35e8ecaea5fe6b045
firstmage <firstmage@163.com>
2019-11-15 17:07:05 +0800

    11.15.17
    test file

--- myworks/data.js
+++ myworks/data.js
@@ -1,125 +1,145 @@
 var myData = {
   designer: {
-    name: '王设计',
-    phone: '688688',
-    tag: ["cool"],
-    bg: '#232323'
+    name: 'Xinyuan Production',
+    phone: '',
+    tag: ["我是王鑫源，平面设计师，现居北京。 我用设计帮助公司解决问题、实现增长。"],
+    bg: '#1e2025'
   },
   recommend: {
     contact: '',
     city: ''
   },
   gallery: [{
-    src: "myworks/0d6abf0465808e5ebff1059c5157f031.jpg",
+    src: "myworks/(1).jpg",
+    w: 1800,
+    h: 1200
+  }, {
+    src: "myworks/2.jpg",
+    w: 1800,
+    h: 1100
+  }, {
+    src: "myworks/3.jpg",
+    w: 1800,
+    h: 1921
+  }, {
+    src: "myworks/4.jpg",
+    w: 1800,
+    h: 1101
+  }, {
+    src: "myworks/5.jpg",
+    w: 1800,
+    h: 1277
+  }, {
+    src: "myworks/6.jpg",
+    w: 1800,
+    h: 976
+  }, {
+    src: "myworks/7.gif",
+    w: 2000,
+    h: 1250
+  }, {
+    src: "myworks/8.jpg",
+    w: 1800,
+    h: 1081
+  }, {
+    src: "myworks/9.1.JPG",
+    w: 1800,
+    h: 1080
+  }, {
+    src: "myworks/9.jpg",
+    w: 1800,
+    h: 1199
+  }, {
+    src: "myworks/10.jpg",
+    w: 1800,
+    h: 1391
+  }, {
+    src: "myworks/11.jpg",
+    w: 1800,
+    h: 1177
+  }, {
+    src: "myworks/12.jpg",
+    w: 1800,
+    h: 1821
+  }, {
+    src: "myworks/13.jpg",
+    w: 1800,
+    h: 1100
+  }, {
+    src: "myworks/14.jpg",
+    w: 1800,
+    h: 1199
+  }, {
+    src: "myworks/15.jpg",
+    w: 1800,
+    h: 1897
+  }, {
+    src: "myworks/16.jpg",
+    w: 1800,
+    h: 1258
+  }, {
+    src: "myworks/21.jpg",
+    w: 1800,
+    h: 840
+  }, {
+    src: "myworks/31.jpg",
+    w: 1800,
+    h: 840
+  }, {
+    src: "myworks/41.jpg",
+    w: 1800,
+    h: 840
+  }, {
+    src: "myworks/51.jpg",
     w: 900,
     h: 600
   }, {
-    src: "myworks/1a11accc9c2c1704b48b95b97616332b.jpg",
-    w: 480,
-    h: 480
-  }, {
-    src: "myworks/1dec71b070680dd5e0970e9163415677.png",
-    w: 579,
-    h: 141
-  }, {
-    src: "myworks/1e6193ff7bbf65a5ec446d63116938dd.png",
-    w: 525,
-    h: 290
-  }, {
-    src: "myworks/1fa8c5bd74a73dc6a9d799bbd9d0855c.jpg",
-    w: 680,
-    h: 392
-  }, {
-    src: "myworks/02ffbb99b43a6869bfbbdd55ace2f3e0.jpg",
-    w: 480,
-    h: 480
-  }, {
-    src: "myworks/2af57fc037834d6ee315a7167f30ebc2.jpg",
-    w: 640,
-    h: 353
-  }, {
-    src: "myworks/2b3d21c3f3aee34a339377a40a453a0e.jpg",
-    w: 900,
+    src: "myworks/61.jpg",
+    w: 1800,
     h: 600
   }, {
-    src: "myworks/2b37f680f2cd21661c87a9cf6beedcb4.png",
-    w: 6,
-    h: 7
+    src: "myworks/71.jpg",
+    w: 1801,
+    h: 601
   }, {
-    src: "myworks/2cb1ae0eedc35f4ffeb173d4f2a6727c.png",
-    w: 406,
-    h: 228
+    src: "myworks/81.jpg",
+    w: 1800,
+    h: 840
   }, {
-    src: "myworks/2cf136d93b2c0ad79fdd72b87526754f.jpg",
-    w: 480,
-    h: 480
+    src: "myworks/91.jpg",
+    w: 1800,
+    h: 840
   }, {
-    src: "myworks/2d08eef543be38c1f92f1d75123c7adf.png",
-    w: 557,
-    h: 151
+    src: "myworks/101.jpg",
+    w: 1800,
+    h: 840
   }, {
-    src: "myworks/0a00bf5b4478cc4bccb13d28652b515e.jpg",
-    w: 900,
-    h: 600
+    src: "myworks/111.jpg",
+    w: 1800,
+    h: 840
   }, {
-    src: "myworks/0c4791d8f641fb07d4f1dbcccc81ebd4.jpg",
-    w: 481,
-    h: 481
+    src: "myworks/121.jpg",
+    w: 1800,
+    h: 3451
   }, {
-    src: "myworks/0fb2155d9cad2a8fa690fc8e8046ec7f.jpg",
-    w: 900,
-    h: 600
+    src: "myworks/131.jpg",
+    w: 1800,
+    h: 1140
   }, {
-    src: "myworks/1a409799f7daced5985a8fdedca6a84a.jpg",
-    w: 481,
-    h: 481
+    src: "myworks/141.jpg",
+    w: 1800,
+    h: 5035
   }, {
-    src: "myworks/1e3a7cd977bd38cf796adf8b89aafcc5.png",
-    w: 405,
-    h: 224
+    src: "myworks/161.jpg",
+    w: 1800,
+    h: 2035
   }, {
-    src: "myworks/02c98a0ed5012d443f02d0780744433c.jpg",
-    w: 900,
-    h: 600
+    src: "myworks/712.jpg",
+    w: 1801,
+    h: 601
   }, {
-    src: "myworks/2c8d38292b04800df5f28ad8cf8a59a6.jpg",
-    w: 481,
-    h: 481
-  }, {
-    src: "myworks/0a062314dfe67216925f29a2b6a48d59.jpg",
-    w: 481,
-    h: 481
-  }, {
-    src: "myworks/0ac4c08c003dfc7b3e50500e7cddba0b.jpg",
-    w: 481,
-    h: 481
-  }, {
-    src: "myworks/0d65eb60531757703718d96800721fd8.jpg",
-    w: 750,
-    h: 617
-  }, {
-    src: "myworks/0e97d94d433d126b481c0f2a80edb6c2.jpg",
-    w: 815,
-    h: 678
-  }, {
-    src: "myworks/0f01742388aeeca6150894af6f43c750.jpg",
-    w: 900,
-    h: 600
-  }, {
-    src: "myworks/1b2d1888db14c64d5f78f173cf517750.jpg",
-    w: 900,
-    h: 600
-  }, {
-    src: "myworks/2a0817b8cefae804ae3ad9e88baec4ea.jpg",
-    w: 480,
-    h: 480
-  }, {
-    src: "myworks/2c7c80b2900a32f4ceec78620a4068d0.jpg",
-    w: 815,
-    h: 678
-  }, {
-    src: "myworks/2ccb9b1c38ab096a56c8b46ce0bbf4a8.jpg",
-    w: 900,
-    h: 600
+    src: "myworks/20170506195306_142.gif",
+    w: 1800,
+    h: 800
   }, ]
 }
